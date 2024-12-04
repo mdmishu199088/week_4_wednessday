024e22e63f00c5444d1b0941995f40185e12a240
mdmishu199088 <mdmishu199088@gmail.com>
2024-12-04 17:00:47 +0600

    Add files via upload
    add first

--- problam_a.cpp
+++ problam_a.cpp
@@ -4,6 +4,30 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        deque<int>ans,v;
+        for(int i=0;i<n;i++){
+            int x;cin>>x;
+            ans.push_back(x);
+            v.push_back(x);
+        }
+        sort(v.begin(),v.end());
+        bool flag=false;
+        while(n--){
+            if(ans==v){
+                flag=true;
+            }
+            else {
+                int x=ans.front();
+                ans.pop_front();
+                ans.push_back(x);
+            }
+        }
+        if(flag)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
     return 0;
 }
+// ok
